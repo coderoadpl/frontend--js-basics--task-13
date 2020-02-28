8d73a15949480ec82044e4bd852380e22908887f
Mateusz <chomamateusz@gmail.com>
2020-02-28 13:15:32 +0100

    Automatic commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+'use strict'
+
+var x = 1
+var y = 2
+
+if (x === y) {
+    console.log('Prawda!')
+} else {
+    console.log('Fałsz!')
+}
+
+if (x !== y) {
+    console.log('Prawda - x NIE jest równy y!')
+} else {
+    console.log('Fałsz - x jest równy y!')
+}
+
+var string = 'hello'
+var emptyString = ''
+
+if (string) {
+    console.log('Prawda!')
+} else {
+    console.log('Fałsz!')
+}
+
+if (emptyString) {
+    console.log('Prawda!')
+} else {
+    console.log('Fałsz!')
+}
+
+// add(0, 0) === 0
+// add(0) === 0
+// add() === 0
+// add(5) === 5
+
+function add(a, b) {
+    console.log('a: ', a)
+    console.log('b: ', b)
+
+    if(a === undefined){
+        a = 0
+    }
+    if(b === undefined){
+        b = 0
+    }
+
+    return Number(a) + Number(b)
+}
